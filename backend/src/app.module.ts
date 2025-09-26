2edce27d208ad37334a26e29904d5acf9ddd93b5
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-26 23:05:48 +0500

    refactor(AppModule): убрал импорты конроллеров и сервисов для films и order

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,6 @@ import { ConfigModule } from '@nestjs/config';
 import * as path from 'node:path';
 
 import { configProvider } from './app.config.provider';
-import { FilmsController } from './films/films.controller';
-import { OrderController } from './order/order.controller';
-import { FilmsService } from './films/films.service';
-import { OrderService } from './order/order.service';
 import { FilmsModule } from './films/films.module';
 import { OrderModule } from './order/order.module';
 
@@ -24,7 +20,7 @@ import { OrderModule } from './order/order.module';
     FilmsModule,
     OrderModule,
   ],
-  controllers: [FilmsController, OrderController],
-  providers: [configProvider, FilmsService, OrderService],
+  controllers: [],
+  providers: [configProvider],
 })
 export class AppModule {}
