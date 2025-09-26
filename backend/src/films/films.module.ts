dc6d8692e564a83c235d20b58331a3d329bf2262
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-27 00:56:09 +0500

    feat(FilmsModule): собрал FilmsModule

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RepositoryModule } from 'src/repository/repository.module';
+import { FilmsController } from './films.controller';
+import { FilmsService } from './films.service';
 
-@Module({})
+@Module({
+  imports: [RepositoryModule],
+  controllers: [FilmsController],
+  providers: [FilmsService],
+})
 export class FilmsModule {}
