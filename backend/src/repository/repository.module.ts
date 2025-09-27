26b28e953721acf1e528fed474cf430c553e2077
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-27 22:28:26 +0500

    feat(RepositoryModule): добавил InMemoryOrderRepository в RepositoryModule

--- backend/src/repository/repository.module.ts
+++ backend/src/repository/repository.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
-import { InMemoryFilmRepository } from './in-memory-film.repository';
 import { AppConfig, configProvider } from 'src/app.config.provider';
 import { IFilmRepository } from 'src/films/film.repository';
+import { InMemoryFilmRepository } from './in-memory-film.repository';
+import { InMemoryOrderRepository } from './in-memory-order.repository';
+import { IOrderRepository } from 'src/order/order.repository';
 
 @Module({
   providers: [
     configProvider,
     InMemoryFilmRepository,
+    InMemoryOrderRepository,
     {
       // Предоставляем IFilmRepository, используя фабрику для условного выбора реализации.
       provide: IFilmRepository,
@@ -16,11 +19,29 @@ import { IFilmRepository } from 'src/films/film.repository';
         if (config.database.driver === 'mongodb') {
           // return mongoRepo;
         }
+
         return inMemoryRepo;
       },
       inject: [configProvider.provide, InMemoryFilmRepository],
     },
+    {
+      // Предоставляем IOrderRepository, используя фабрику для условного выбора реализации.
+      provide: IOrderRepository,
+      useFactory: (
+        config: AppConfig,
+        inMemoryRepo: InMemoryOrderRepository,
+      ) => {
+        // Если драйвер MongoDB, вернется соответствующий репозиторий (закомментировано).
+        // Иначе используется In-Memory репозиторий.
+        if (config.database.driver === 'mongodb') {
+          // return mongoRepo;
+        }
+
+        return inMemoryRepo;
+      },
+      inject: [configProvider.provide, InMemoryOrderRepository],
+    },
   ],
-  exports: [IFilmRepository],
+  exports: [IFilmRepository, IOrderRepository],
 })
 export class RepositoryModule {}
